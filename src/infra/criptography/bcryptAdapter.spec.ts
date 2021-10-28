631ef592bb23620cfa602f1166990ee34507185f
Yuri Castro <yuricastro51@gmail.com>
2021-10-27 21:35:54 -0300

    test: ensure BcryptAdapter returns a hash on success

--- src/infra/criptography/bcryptAdapter.spec.ts
+++ src/infra/criptography/bcryptAdapter.spec.ts
@@ -14,11 +14,23 @@ const makeSut = (): SutTypes => {
   return { sut, salt };
 };
 
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return 'hash';
+  }
+}));
+
 describe('Bcrypt Adapter', () => {
   test('Should call Bcrypt with correct value', async () => {
     const { sut, salt } = makeSut();
     const hashSpy = jest.spyOn(bcrypt, 'hash');
     await sut.encrypt('any_value');
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt);
+  });
+
+  test('Should return a hash on success', async () => {
+    const { sut } = makeSut();
+    const hash = await sut.encrypt('any_value');
+    expect(hash).toBe('hash');
   });
 });
