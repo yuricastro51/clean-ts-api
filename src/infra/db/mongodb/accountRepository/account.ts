82a80360cc5246f208ebdadf8797ccdcdf25b20a
Yuri Castro <yuricastro51@gmail.com>
2021-10-28 22:54:56 -0300

    refactor: move mapping to MongoHelper

--- src/infra/db/mongodb/accountRepository/account.ts
+++ src/infra/db/mongodb/accountRepository/account.ts
@@ -7,9 +7,7 @@ export class AccountMongoRepository implements AddAccountRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = MongoHelper.getCollection('accounts');
     const result = await accountCollection.insertOne(accountData);
-    const foundAccount: any = await accountCollection.findOne({ _id: result.insertedId });
-    const { _id, ...accountWithoutId } = foundAccount;
-    const account = Object.assign({}, accountWithoutId, { id: _id });
-    return account;
+    const account: any = await accountCollection.findOne({ _id: result.insertedId });
+    return MongoHelper.map(account);
   }
 }
