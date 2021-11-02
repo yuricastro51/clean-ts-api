1554baebdb81f5ad5f90acc87b7cfcba6d3722d9
Yuri Castro <yuricastro51@gmail.com>
2021-11-02 11:04:12 -0300

    feat: add LogMongoRepository to SignUpFactory

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,10 +1,11 @@
 import { DbAddAccount } from '../../data/usecases/addAccount/dbAddAccount';
 import { BcryptAdapter } from '../../infra/criptography/bcryptAdapter';
 import { AccountMongoRepository } from '../../infra/db/mongodb/accountRepository/account';
+import { LogMongoRepository } from '../../infra/db/mongodb/logRepository/log';
 import { SignUpController } from '../../presentation/controllers/signup';
 import { Controller } from '../../presentation/protocols/controller';
 import { EmailValidatorAdapter } from '../../utils/emailValidatorAdapter';
-// import { LogControllerDecorator } from '../decorators/log';
+import { LogControllerDecorator } from '../decorators/log';
 
 export const makeSignUpController = (): Controller => {
   const salt = 12;
@@ -13,7 +14,7 @@ export const makeSignUpController = (): Controller => {
   const bcryptAdapter = new BcryptAdapter(salt);
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository);
   const signUpController = new SignUpController(emailValidator, dbAddAccount);
-  // const logControllerDecorator = new LogControllerDecorator(signUpController);
-  return signUpController;
-  // return logControllerDecorator;
+  const logMongoRepository = new LogMongoRepository();
+  const logControllerDecorator = new LogControllerDecorator(signUpController, logMongoRepository);
+  return logControllerDecorator;
 };
